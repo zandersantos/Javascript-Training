2e640bdbff47c476dcf3b83a87de179bcbcec592
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 11:33:26 -0500

    Used basic Logical Operators in If statements and logged them in the console

--- sandbox3.js
+++ sandbox3.js
@@ -73,17 +73,34 @@
 
 //Tutorial 3.5
 //If Else Statements
-const password = 'p@ssword1234';
+// const password = 'p@ssword1234';
 
-if(password.length >= 12)
+// if(password.length >= 12)
+// {
+//   console.log('that password is mighty strong')
+// }
+// else if(password.length >= 8)
+// {
+//   console.log('that password is long enough..');
+// }
+// else
+// {
+//   console.log('password is not long enough');
+// }
+
+//Tutorial 3.6
+//Logical Operators - OR || and AND &&
+const password = 'p@ssor';
+
+if(password.length >= 12 && password.includes('@'))
 {
   console.log('that password is mighty strong')
 }
-else if(password.length >= 8)
+else if(password.length >= 8 || password.includes('@') && password.length > 5)
 {
-  console.log('that password is long enough..');
+  console.log('that password is strong enough...');
 }
 else
 {
-  console.log('password is not long enough');
+  console.log('password is not strong enough');
 }
