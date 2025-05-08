168fcde9213d31997e424c5d38ad1ac3ea2f7031
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 09:17:28 -0500

    Added variable logging to sandbox.js

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,19 @@
-console.log(1);
-console.log(2);
+// Tutorial #2.1
+// console.log(1);
+// console.log(2);
+
+//Tutorial #2.2
+let age = 25;
+let year = 2019;
+
+//Log age and year to console
+console.log(age, year);
+
+age = 30;
+console.log(age);
+
+const points = 100;
+console.log(points);
+
+var score = 75;
+console.log(score);
