53e25e39de5e513df85e6bdef8a12c4b15710464
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 15:15:54 -0500

    Used querySelectorAll to get all of specific tags including p and .error, and logged them in the console

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,25 @@
 //Tutorial 6 - The Document Object Model
 
-//Grabs ONLY the first p tag
-const para = document.querySelector('p');
-console.log(para);
+// //Grabs ONLY the first p tag
+// const para = document.querySelector('p');
+// console.log(para);
 
-//Grabs the tag with the error class
-const para2 = document.querySelector('.error');
-console.log(para2);
+// //Grabs the tag with the error class
+// const para2 = document.querySelector('.error');
+// console.log(para2);
 
-//Grabs a div tag with the error class
-const para3 = document.querySelector('div.error');
-console.log(para3);
+// //Grabs a div tag with the error class
+// const para3 = document.querySelector('div.error');
+// console.log(para3);
+
+const paras = document.querySelectorAll('p');
+
+console.log(paras[0]);
+
+paras.forEach(para =>
+{
+  console.log(para);
+});
+
+const errors = document.querySelectorAll('.error');
+console.log(errors);
