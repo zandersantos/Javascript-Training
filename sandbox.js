8b24172e6cc502e6fba2f309691b600e0cf43e4c
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 15:20:52 -0500

    Used getElementBy Id, ClassName and TagName and logged them in the console

--- sandbox.js
+++ sandbox.js
@@ -1,5 +1,6 @@
 //Tutorial 6 - The Document Object Model
 
+//Tutorial 6.1
 // //Grabs ONLY the first p tag
 // const para = document.querySelector('p');
 // console.log(para);
@@ -12,14 +13,33 @@
 // const para3 = document.querySelector('div.error');
 // console.log(para3);
 
-const paras = document.querySelectorAll('p');
+// const paras = document.querySelectorAll('p');
 
-console.log(paras[0]);
+// console.log(paras[0]);
 
-paras.forEach(para =>
-{
-  console.log(para);
-});
+// paras.forEach(para =>
+// {
+//   console.log(para);
+// });
 
-const errors = document.querySelectorAll('.error');
+// const errors = document.querySelectorAll('.error');
+// console.log(errors);
+
+
+//Tutorial 6.2
+
+//Get Element by ID
+const title = document.getElementById('page-title');
+console.log(title);
+
+//Get Elements by Their class name
+const errors = document.getElementsByClassName('error');
 console.log(errors);
+console.log(errors[0]);
+            //You cannot do forEach
+
+
+//Get Elements by their tag name
+const paras = document.getElementsByTagName('p');
+console.log(paras);
+console.log(paras[1]);
