d652e57ddfe01de44b6454a93e13f46e348a2b8b
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 13:20:53 -0500

    Created function expressions with arguments and statements with parameters and logged them in the console

--- sandbox4.js
+++ sandbox4.js
@@ -2,21 +2,34 @@
 
 //Tutorial 4.1
 
-//Function Expression
-const speak = function()
+// //Function Expression
+// const speak = function()
+// {
+//   console.log('good day!?')
+// }; //You need a semi-colon for an expression
+
+// greet();
+// greet();
+// greet();
+
+// speak();
+// speak();
+
+// //Function Declaration
+// function greet()
+// {
+//   console.log('hello there');
+// }
+
+
+//Tutorial 4.2
+
+//Arguments and Parameters
+const speak = function(name ='luigi', time = 'night')
 {
-  console.log('good day!?')
+  console.log(`good ${time} ${name}!?`)
 }; //You need a semi-colon for an expression
 
-greet();
-greet();
-greet();
-
 speak();
-speak();
-
-//Function Declaration
-function greet()
-{
-  console.log('hello there');
-}
+speak("mario", "morning");
+speak("toad");
