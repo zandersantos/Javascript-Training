8ec10852097d48a4f5151523c0657f5605b874a0
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 15:14:16 -0500

    Used querySelector to grab html information from index.html and logged them in the console

--- sandbox.js
+++ sandbox.js
@@ -1,2 +1,13 @@
-//Tutorial 6 - The Document Objext Model
+//Tutorial 6 - The Document Object Model
 
+//Grabs ONLY the first p tag
+const para = document.querySelector('p');
+console.log(para);
+
+//Grabs the tag with the error class
+const para2 = document.querySelector('.error');
+console.log(para2);
+
+//Grabs a div tag with the error class
+const para3 = document.querySelector('div.error');
+console.log(para3);
