171b762d247d9924806169d80c6672a4e1333397
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 13:15:45 -0500

    Created function expression and function declaration statements and logged them to the console

---
+++ sandbox4.js
@@ -0,0 +1,22 @@
+//Tutorial 4: Functions
+
+//Tutorial 4.1
+
+//Function Expression
+const speak = function()
+{
+  console.log('good day!?')
+}; //You need a semi-colon for an expression
+
+greet();
+greet();
+greet();
+
+speak();
+speak();
+
+//Function Declaration
+function greet()
+{
+  console.log('hello there');
+}
