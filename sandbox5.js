137f6f6d4f0357609ea3d3840b87e955f7559322
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 14:48:44 -0500

    Compared changing Primitive and Reference values

--- sandbox5.js
+++ sandbox5.js
@@ -86,33 +86,57 @@ let user =
 //Tutorial 5.2
 //Math Object
 
-console.log(Math);
-console.log(Math.PI);
-console.log(Math.E);
+// console.log(Math);
+// console.log(Math.PI);
+// console.log(Math.E);
 
-const area = 7.8;
-console.log(Math.round(area));
-//No matter what the decimal is, brings it down to the whole number
-console.log(Math.floor(area));
+// const area = 7.8;
+// console.log(Math.round(area));
+// //No matter what the decimal is, brings it down to the whole number
+// console.log(Math.floor(area));
 
-//No matter what the decimal is, brings it up to the whole number
-console.log(Math.ceil(area));
+// //No matter what the decimal is, brings it up to the whole number
+// console.log(Math.ceil(area));
 
-//Removes the decimal points
-console.log(Math.trunc(area));
+// //Removes the decimal points
+// console.log(Math.trunc(area));
 
-//Random Numbers
+// //Random Numbers
 
-//Always between 0 & 1
-const random = Math.random();
-console.log(random);
+// //Always between 0 & 1
+// const random = Math.random();
+// console.log(random);
 
-//Either 1 or 0
-console.log(Math.round(random));
+// //Either 1 or 0
+// console.log(Math.round(random));
 
-//Always between 1 & 100
-console.log(random*100);
+// //Always between 1 & 100
+// console.log(random*100);
 
+
+//Primitive vs Reference
+/*
+Primitive is stored in a Stack.
+- Stack space is limited.
+Reference is stored in a Heap.
+- Can hold unique and complex values but can be slow
+*/
+
+//Primitive Values
+let scoreOne = 50;
+let scoreTwo = scoreOne;
+
+console.log(`scoreOne: ${scoreOne}`, `scoreTwo: ${scoreTwo}`);
+
+
+//Reference Values
+const userOne = {name: 'ryu', age: 30};
+const userTwo = userOne;
+
+console.log(userOne, userTwo);
+
+userOne.age = 50;
+console.log(userOne, userTwo); //they are both the same value now, because only their pointers were updated
 
 
 
