3fff72bbca216ecea190239b654970895572daee
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 09:43:48 -0500

    Created string variables and logged them to the console

--- sandbox.js
+++ sandbox.js
@@ -3,17 +3,44 @@
 // console.log(2);
 
 //Tutorial #2.2
-let age = 25;
-let year = 2019;
+// let age = 25;
+// let year = 2019;
 
-//Log age and year to console
-console.log(age, year);
+// //Log age and year to console
+// console.log(age, year);
 
-age = 30;
-console.log(age);
+// age = 30;
+// console.log(age);
 
-const points = 100;
-console.log(points);
+// const points = 100;
+// console.log(points);
 
-var score = 75;
-console.log(score);
+// var score = 75;
+// console.log(score);
+
+//Strings
+console.log("Hello, World!");
+
+let email = 'mario@thenetninja.co.uk';
+console.log(email);
+
+//Strings Concatenation
+let firstName = 'Brandon';
+let lastName = 'Sanderson';
+
+let fullName = firstName + " " + lastName;
+console.log(fullName);
+
+//Getting Characters
+console.log(fullName[8]);
+
+//String Length
+console.log(fullName.length);
+
+//String Methods
+console.log(fullName.toUpperCase());
+let result = fullName.toLowerCase();
+console.log(result, fullName);
+
+let index = email.indexOf('@');
+console.log(index);
