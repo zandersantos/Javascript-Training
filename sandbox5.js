cde89b37703d4f3b19aab34164c7789f3a193422
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 14:38:19 -0500

    Created Math object statements and logged with to the console

--- sandbox5.js
+++ sandbox5.js
@@ -46,7 +46,7 @@ let user =
 };
 
 //We want this.
-user.logBlogs();
+//user.logBlogs();
 
 //We dont want this. This just gets the Window.
 // console.log(this);
@@ -80,3 +80,39 @@ user.logBlogs();
 
 // //Getting the type of an object
 // console.log(typeof user);
+
+
+
+//Tutorial 5.2
+//Math Object
+
+console.log(Math);
+console.log(Math.PI);
+console.log(Math.E);
+
+const area = 7.8;
+console.log(Math.round(area));
+//No matter what the decimal is, brings it down to the whole number
+console.log(Math.floor(area));
+
+//No matter what the decimal is, brings it up to the whole number
+console.log(Math.ceil(area));
+
+//Removes the decimal points
+console.log(Math.trunc(area));
+
+//Random Numbers
+
+//Always between 0 & 1
+const random = Math.random();
+console.log(random);
+
+//Either 1 or 0
+console.log(Math.round(random));
+
+//Always between 1 & 100
+console.log(random*100);
+
+
+
+
