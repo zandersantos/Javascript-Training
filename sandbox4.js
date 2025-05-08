995cbfadfdc962e3c0f0c17b34467d2d5588b9e7
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 13:34:54 -0500

    Created Arrow Function Statements and logged them to the console

--- sandbox4.js
+++ sandbox4.js
@@ -35,14 +35,56 @@
 // speak("toad");
 
 //Tutorial 4.3
-//Returning Values
+// Returning Values
 
-const calcArea = function(radius)
-{
-  // let area = 3.14 * radius**2;
-  // return area;
-  return 3.14 * radius**2;
-};
+// const calcArea = function(radius)
+// {
+//   // let area = 3.14 * radius**2;
+//   // return area;
+//   return 3.14 * radius**2;
+// };
+
+// const areaGiven = calcArea(5);
+// console.log(areaGiven);
+
+//Tutorial 4.4
+//Regular Funciton
+// const calcArea = function(radius)
+// {
+//   return 3.14 * radius**2;
+// };
+
+//Arrow Function (Shorten the Function Expression)
+const calcArea = (radius) => 3.14 * radius**2;
 
 const areaGiven = calcArea(5);
-console.log(areaGiven);
+console.log('area is: ', areaGiven);
+
+//PRACTICE
+const greet1 = function()
+{
+  return 'hello';
+};
+
+const greet = () => 'hello';
+
+const bill1 = function(product, tax)
+{
+  let total = 0;
+  for(let i = 0; i < product.length; i++)
+  {
+    total += product[i] + product[i] * tax;
+  }
+  return total;
+}
+
+//Not Much Difference
+const bill = (product, tax) =>
+{
+  let total = 0;
+  for(let i = 0; i < product.length; i++)
+  {
+    total += product[i] + product[i] * tax;
+  }
+  return total;
+}
