0249ea300b0f51d3d6bc8fb8adaabcc50d783ff9
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 13:26:06 -0500

    Created Function Returning Values statements and logged them to the console

--- sandbox4.js
+++ sandbox4.js
@@ -24,12 +24,25 @@
 
 //Tutorial 4.2
 
-//Arguments and Parameters
-const speak = function(name ='luigi', time = 'night')
-{
-  console.log(`good ${time} ${name}!?`)
-}; //You need a semi-colon for an expression
+// //Arguments and Parameters
+// const speak = function(name ='luigi', time = 'night')
+// {
+//   console.log(`good ${time} ${name}!?`)
+// }; //You need a semi-colon for an expression
 
-speak();
-speak("mario", "morning");
-speak("toad");
+// speak();
+// speak("mario", "morning");
+// speak("toad");
+
+//Tutorial 4.3
+//Returning Values
+
+const calcArea = function(radius)
+{
+  // let area = 3.14 * radius**2;
+  // return area;
+  return 3.14 * radius**2;
+};
+
+const areaGiven = calcArea(5);
+console.log(areaGiven);
