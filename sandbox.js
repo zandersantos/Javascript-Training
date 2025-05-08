731e633ad0313e407067c39f6a9d6fadaabd8d21
Zander Santos <143445073+zandersantos@users.noreply.github.com>
2025-05-08 09:49:39 -0500

    Created common string methods and logged them to the console

--- sandbox.js
+++ sandbox.js
@@ -19,28 +19,39 @@
 // console.log(score);
 
 //Strings
-console.log("Hello, World!");
+// console.log("Hello, World!");
 
-let email = 'mario@thenetninja.co.uk';
-console.log(email);
+// let email = 'mario@thenetninja.co.uk';
+// console.log(email);
 
-//Strings Concatenation
-let firstName = 'Brandon';
-let lastName = 'Sanderson';
+// //Strings Concatenation
+// let firstName = 'Brandon';
+// let lastName = 'Sanderson';
 
-let fullName = firstName + " " + lastName;
-console.log(fullName);
+// let fullName = firstName + " " + lastName;
+// console.log(fullName);
 
-//Getting Characters
-console.log(fullName[8]);
+// //Getting Characters
+// console.log(fullName[8]);
 
-//String Length
-console.log(fullName.length);
+// //String Length
+// console.log(fullName.length);
 
-//String Methods
-console.log(fullName.toUpperCase());
-let result = fullName.toLowerCase();
-console.log(result, fullName);
+// //String Methods
+// console.log(fullName.toUpperCase());
+// let result = fullName.toLowerCase();
+// console.log(result, fullName);
 
-let index = email.indexOf('@');
-console.log(index);
+// let index = email.indexOf('@');
+// console.log(index);
+
+// //Common string Methods
+//let email = "mario@thenetninja.co.uk";
+
+//let result = email.lastIndexOf('n');
+//let result = email.slice(2,5);
+//let result = email.substr(4,10);
+//let result = email.replace('m', 'w');
+//let result = email.replace('n', 'w');
+
+//console.log(result);
